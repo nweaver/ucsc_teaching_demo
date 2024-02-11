101596eba100ce6329637608a9ddfdfa9b8104ce
Nicholas Weaver <nweaver@icsi.berkeley.edu>
2024-02-10 17:41:52 -0800

    Checkpoint

--- C++/Graph.h
+++ C++/Graph.h
@@ -12,14 +12,21 @@ template <class T> class GraphNode;
 
 template <class T>
 class GraphEdge {
-    GraphNode<T> *start;
-    GraphNode<T> *end;
+public:
+    std::shared_ptr<GraphNode<T>> start;
+    std::shared_ptr<GraphNode<T>> end;
     double weight;
-    GraphEdge(GraphNode<T> *startIn, GraphNode<T> *endIn,
+    GraphEdge(std::shared_ptr<GraphNode<T>> startIn,
+              std::shared_ptr<GraphNode<T>> endIn,
         double weightIn) {
         start = startIn;
         end = endIn;
         weight = weightIn;
+        for(auto itr: start->outEdges) {
+            if(itr->end == end) {
+                throw std::domain_error("Edge asready exists");
+            }
+        }
     }
 };
 
@@ -27,39 +34,32 @@ template <class T>
 class GraphNode {
 public:
     T name;
-    std::unordered_set<GraphEdge<T>> outEdges {};
-    std::unordered_set<GraphEdge<T>> inEdges {};
+    std::unordered_set<std::shared_ptr<GraphEdge<T>>> outEdges {};
+    std::unordered_set<std::shared_ptr<GraphEdge<T>>> inEdges {};
     explicit GraphNode(T nameIn) {
         name = nameIn;
     }
 
-    void connectTo( GraphNode<T>* dest, double weight) {
-        for (auto itr: outEdges) {
-            if(itr.end == dest) {
-                throw std::domain_error("Edge already exists");
-            }
-        }
-        auto edge = new GraphEdge(this, dest, weight);
-        this->outEdges.insert(edge);
-        dest->inEdges.insert(edge);
-    }
-
     /*
      * Note: This does NOT delete the node but also deletes the
-     * out edges and in-edges.  This is needed for the C++ version
+     * out edges.  This is needed for the C++ version
      * but not the python or kotlin versions of the application.
+     *
+     * Because graphs contain cycles, the reference counting abilities
+     * of std::shared_ptr do not help here, instead we have to manually
+     * do this.
      */
     ~GraphNode() {
         for(auto itr : outEdges) {
-            auto other = itr.end;
+            auto other = itr->end;
             other->inEdges.erase(itr);
-            delete itr;
         }
         for(auto itr : inEdges) {
-            auto other = itr.start;
+            auto other = itr->start;
             other->outEdges.erase(itr);
-            delete itr;
         }
+        inEdges.clear();
+        outEdges.clear();
     }
 };
 
@@ -69,9 +69,24 @@ public:
 template <class T>
 class Graph {
 protected:
-    std::map<T, std::shared_ptr<GraphNode<T>>> nodes {};
+    std::unordered_map<T, std::shared_ptr<GraphNode<T>>> nodes {};
 public:
+    void createNode(T name) {
+        if(nodes.contains(name)) {
+            throw std::domain_error("Node already exists");
+        }
+        nodes[name] = std::make_shared<GraphNode<T>>(name);
+    }
 
+    void createLink(T start, T end, double weight) {
+        if(!nodes.contains(start) || !nodes.contains(end)) {
+            throw std::domain_error("Node does not exist");
+        }
+        auto edge = std::make_shared<GraphEdge<T>>(nodes[start], nodes[end],
+            weight);
+        nodes[start]->outEdges.insert(edge);
+        nodes[end]->inEdges.insert(edge);
+    }
 };
 
 /*
@@ -90,16 +105,81 @@ struct DijkstraIterationStep {
     }
 };
 
+
+
 template <class T>
 class DijkstraTraversal {
-    DijkstraTraversal(std::shared_ptr<Graph<T>> G, T start) {
-        std::unordered_set<std::shared_ptr<DijkstraIterationStep<T>>> workingSet;
-        for(auto itr : G->nodes) {
-            workingSet.insert(std::make_shared<DijkstraIterationStep<T>>(
-                itr));
-        }
 
+    std::unordered_map<std::shared_ptr<GraphNode<T>>,
+                        std::shared_ptr<DijkstraIterationStep<T>>> workingSet;
+    std::shared_ptr<DijkstraIterationStep<T>> currentNode = nullptr;
+
+    DijkstraTraversal(std::shared_ptr<Graph<T>> G, T start) {
+        if(!G.nodes.contains(start)) {
+            throw std::logic_error("Unable to find the node");
+        }
+        for(auto itr : G->nodes) {
+            auto element = std::make_shared<DijkstraIterationStep<T>>(itr.second);
+            if (itr.first == start) {
+                element->distance = 0;
+                currentNode = element;
+            } else {
+                workingSet[itr.second] = element;
+            }
+        }
     }
+
+    DijkstraTraversal& begin() {
+        return &this;
+    }
+
+    DijkstraTraversal& end() {
+        return &this;
+    }
+
+
+    std::shared_ptr<DijkstraIterationStep<T>> operator*() {
+        return currentNode;
+    }
+
+
+    // The ++ operator is the part that does the
+    void operator++(){
+
+        currentNode = nullptr;
+        if (workingSet.size() == 0) {
+            return;
+        }
+        for (auto itr: workingSet) {
+            if(currentNode == nullptr) {
+                currentNode = itr.second;
+            }
+            if(itr.second->distance < currentNode->distance) {
+                currentNode = itr;
+            }
+        }
+        workingSet.erase(currentNode->current);
+        if(currentNode->distance == HUGE_VAL) {
+            currentNode = nullptr;
+            return;
+        }
+        for (auto itr : currentNode->current->outEdges) {
+            if(workingSet.contains(itr.end)) {
+                auto distance = currentNode->distance + itr->weight;
+                if(distance < workingSet[itr->end].distance) {
+                    workingSet[itr->end].distance = distance;
+                    workingSet[itr->end].previous = currentNode;
+                }
+            }
+        }
+    }
+
+    void operator!=(DijkstraTraversal &other) {
+        assert(other == this);
+        return currentNode == nullptr || workingSet.size() == 0;
+    }
+
+
 };
 
 
--- C++/main.cpp
+++ C++/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
-
+#include "Graph.h"
 
 int main() {
+    auto g = Graph<int>();
+    g.createNode(0);
+    g.createNode(1);
+    g.createLink(0, 1, 1.0);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
