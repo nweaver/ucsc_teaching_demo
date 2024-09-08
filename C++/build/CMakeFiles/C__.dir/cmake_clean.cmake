0057fdb72a3103390d09f2828a5a5fd68b59137c
Nick Weaver <ncweaver@ucdavis.edu>
2024-09-08 00:08:00 +0000

    done

---
+++ C++/build/CMakeFiles/C__.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__.dir/graph.cpp.o"
+  "CMakeFiles/C__.dir/graph.cpp.o.d"
+  "CMakeFiles/C__.dir/main.cpp.o"
+  "CMakeFiles/C__.dir/main.cpp.o.d"
+  "C__"
+  "C__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
